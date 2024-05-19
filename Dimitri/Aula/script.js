c266964b2475c9fcbc7b6fbf43067f2ff3ff4c8b
Luciano <140513755+LucianoAO@users.noreply.github.com>
2024-05-18 22:35:00 -0300

    .script

---
+++ Dimitri/Aula/script.js
@@ -0,0 +1,11 @@
+var p1 = document.createElement('p')
+p1.innerHTML = `texto <strong>JS</strong>`
+document.body.appendChild(p1)
+var p2 = document.createElement('p')
+p2.innerText = 'Escrito com InnerText'
+document.body.append(p2)
+
+console.log('Isso é um alerta no cconsole.log')
+alert('Isto é um alerta')
+console.log(5 + 5)
+
