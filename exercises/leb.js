7691ddfbba8f49471be470ff8dd9a14e785498de
Luciano <140513755+LucianoAO@users.noreply.github.com>
2024-12-08 21:38:05 -0300

    .

---
+++ exercises/leb.js
@@ -0,0 +1,9 @@
+function walterwhite(jesse) {
+    if (jesse == 0 || jesse == Math.sign()) {
+        console.log('jesse esta morto')
+    } else {
+        console.log('Jesse esta vivo')
+    }
+}
+
+console.log(walterwhite(-50))
